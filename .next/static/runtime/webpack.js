fc0621ca79e4bbe050f1bab0f085feba97ac3005
CodeAddictDipesh <dipeshonblack@gmail.com>
2019-10-01 19:42:03 +0545

    basic finished

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7b74897ee58eb0bf7da7";
+/******/ 	var hotCurrentHash = "13696bd8d81071f28078";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
